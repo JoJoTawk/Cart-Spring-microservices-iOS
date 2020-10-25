92e090a46338075b0183417b3df31c5888b830e0
Jhonny <Jhonny.tawk@mcloudagency.com>
2020-10-26 00:15:54 +0200

    Adding Product request and model

---
+++ iOS-SpringBoot-microservices-cart/product-service/src/main/java/com/tawk/product/model/request/ProductRequest.java
@@ -0,0 +1,18 @@
+package com.tawk.product.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequest {
+    private String productCode;
+    private String productTitle;
+    private String imageUrl;
+    private Double discountOffer;
+    private Double price;
+}
